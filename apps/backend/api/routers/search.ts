f3108054ee63c5cc975855e1a3935bbe287c053a
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-12-09 12:50:51 +0100

    Add db search by query & normalize scores

--- apps/backend/api/routers/search.ts
+++ apps/backend/api/routers/search.ts
@@ -1,14 +1,35 @@
+import { ORPCError } from "@orpc/server";
+import { sql } from "drizzle-orm";
 import * as z from "zod";
-import { parsedArticles } from "../../lib/utils/parser.js";
+import { db } from "../../lib/db/index.js";
+import { articlesTable } from "../../lib/db/schema/index.js";
 import { publicProcedure } from "../index.js";
 
 const outputSchema = z.array(
 	z.object({
 		id: z.number(),
 		name: z.string(),
-		wordCounts: z.record(z.string(), z.number()),
+		wordFrequency: z.number(),
 	}),
 );
+
+interface ArticleByQuery {
+	id: number;
+	name: string;
+	wordFrequency: number;
+}
+
+function normalizeScore(articles: ArticleByQuery[]): ArticleByQuery[] {
+	const max = Math.max(
+		Math.max(...articles.map((article) => article.wordFrequency)),
+		1,
+	);
+	return articles.map((article) => ({
+		id: article.id,
+		name: article.name,
+		wordFrequency: article.wordFrequency / max,
+	}));
+}
 
 export const searchRouter = {
 	search: publicProcedure
@@ -16,10 +37,45 @@ export const searchRouter = {
 		.input(
 			z.object({
 				force: z.coerce.boolean().optional().default(false),
+				query: z.string(),
 			}),
 		)
 		.output(outputSchema)
-		.handler(async () => {
-			return parsedArticles.slice(0, 10);
+		.handler(async ({ input }) => {
+			console.log("query", input.query);
+
+			const articles = await db
+				.select()
+				.from(articlesTable)
+				.where(sql`(${articlesTable.wordCounts}->${input.query})::int > 0`);
+
+			if (articles.length === 0) {
+				throw new ORPCError("NOT_FOUND", {
+					message: "No matching articles found",
+				});
+			}
+
+			const sortedArticles = articles.sort((a, b) => {
+				return (
+					(b.wordCounts as Record<string, number>)[input.query] -
+					(a.wordCounts as Record<string, number>)[input.query]
+				);
+			});
+			const normalizedScores = normalizeScore(
+				sortedArticles.map((article) => {
+					const score = (article.wordCounts as Record<string, number>)[
+						input.query
+					];
+					return {
+						id: article.id,
+						name: article.name,
+						wordFrequency: score,
+					};
+				}),
+			);
+
+			console.log("normalizedScores", normalizedScores);
+
+			return normalizedScores;
 		}),
 };
